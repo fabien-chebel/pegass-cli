3d5df27709393c6429fe721df3add5f1616edeeb
Fabien Chebel <fabien+github@chebel.fr>
2022-06-13 22:47:51 +0200

    Send SAMU activity status over WhatsApp (#8)
    This PR adds a command to compute SAMU activities status for the next day.
    
    The summary of the activity can optionally be sent to a WhatsApp group.
    
    Using WhatsApp requires:
    
    1. Registering the device using the `register-chat-device` command
    2. Identifying the group to send notifications to using the `list-chat-groups` command
    3. Adding this groupId to a `whatsapp_notification_group` property in `config.json`

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/fabien-chebel/pegass-cli
 go 1.14
 
 require (
+	github.com/mattn/go-sqlite3 v1.14.13
+	github.com/mdp/qrterminal/v3 v3.0.0
 	github.com/pquerna/otp v1.3.0
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	go.mau.fi/whatsmeow v0.0.0-20220604174810-f28d984f1b9a
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2
+	google.golang.org/protobuf v1.28.0
 	gopkg.in/urfave/cli.v1 v1.20.0
 )
--- redcross/activity-payloads.go
+++ redcross/activity-payloads.go
@@ -1,5 +1,10 @@
 package redcross
 
+import (
+	"strings"
+	"time"
+)
+
 type SeanceList struct {
 	Content          []Seance    `json:"content"`
 	Last             bool        `json:"last"`
@@ -25,10 +30,10 @@ type Seance struct {
 		Tri      string `json:"tri"`
 		CanAdmin bool   `json:"canAdmin"`
 	} `json:"groupeAction"`
-	Debut          string `json:"debut"`
-	Fin            string `json:"fin"`
-	Adresse        string `json:"adresse"`
-	RevisionNumber int    `json:"revisionNumber"`
+	Debut          PegassTime `json:"debut"`
+	Fin            PegassTime `json:"fin"`
+	Adresse        string     `json:"adresse"`
+	RevisionNumber int        `json:"revisionNumber"`
 	RoleConfigList []struct {
 		ID       string `json:"id"`
 		Code     string `json:"code"`
@@ -68,15 +73,87 @@ type InscriptionList []struct {
 	Seance struct {
 		ID string `json:"id"`
 	} `json:"seance"`
-	Utilisateur struct {
-		ID     string `json:"id"`
-		Actif  bool   `json:"actif"`
-		Mineur bool   `json:"mineur"`
-	} `json:"utilisateur"`
-	Debut      string `json:"debut"`
-	Fin        string `json:"fin"`
-	Statut     string `json:"statut"`
-	Role       string `json:"role"`
-	Type       string `json:"type"`
-	IsMultiple bool   `json:"isMultiple"`
+	Utilisateur Utilisateur `json:"utilisateur"`
+	Debut       string      `json:"debut"`
+	Fin         string      `json:"fin"`
+	Statut      string      `json:"statut"`
+	Role        string      `json:"role"`
+	Type        string      `json:"type"`
+	IsMultiple  bool        `json:"isMultiple"`
+}
+
+type Activity struct {
+	ID                      string       `json:"id"`
+	Libelle                 string       `json:"libelle"`
+	StructureOrganisatrice  Structure    `json:"structureOrganisatrice"`
+	StructureMenantActivite Structure    `json:"structureMenantActivite"`
+	Statut                  string       `json:"statut"`
+	SeanceList              []Seance     `json:"seanceList"`
+	TypeActivite            TypeActivite `json:"typeActivite"`
+	Responsable             Utilisateur  `json:"responsable"`
+}
+
+type TypeActivite struct {
+	ID      int    `json:"id"`
+	Libelle string `json:"libelle"`
+	Action  Action `json:"action"`
+}
+
+type Action struct {
+	ID      int    `json:"id"`
+	Libelle string `json:"libelle"`
+}
+
+type ByActivity []Activity
+
+func (a ByActivity) Len() int {
+	return len(a)
+}
+
+func (a ByActivity) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a ByActivity) Less(i, j int) bool {
+	// DAUPHIN < CASTOR < RUBIS < SAPHIR < BABETTE < REGULATION
+	var first = a[i]
+	var second = a[j]
+
+	if ACTIVITY_MAP[first.Libelle] == ACTIVITY_MAP[second.Libelle] {
+		if len(first.SeanceList) > 0 && len(second.SeanceList) > 0 {
+			return time.Time(first.SeanceList[0].Debut).Before(time.Time(second.SeanceList[0].Debut))
+		} else {
+			return true
+		}
+	} else {
+		return ACTIVITY_MAP[first.Libelle] < ACTIVITY_MAP[second.Libelle]
+	}
+
+}
+
+var ACTIVITY_MAP = map[string]int{"01-DAUPHIN": 1,
+	"02-CASTOR": 2, "03-RUBIS": 3, "04-SAPHIR": 4,
+	"05-BABETTE": 5, "REGULATION": 6}
+
+type PegassTime time.Time
+
+func (p *PegassTime) UnmarshalJSON(b []byte) error {
+	value := strings.Trim(string(b), `"`)
+	if value == "" {
+		return nil
+	}
+
+	const layout = "2006-01-02T15:04:05"
+	t, err := time.Parse(layout, value)
+	if err != nil {
+		return err
+	}
+	*p = PegassTime(t)
+	return nil
+}
+
+func (p *PegassTime) PrintTimePart() string {
+	t := time.Time(*p)
+	return t.Format("15:04")
+
 }
